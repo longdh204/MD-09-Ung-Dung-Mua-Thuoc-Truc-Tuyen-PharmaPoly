5c957d70ac49affef6ca241112bbc46c407a4c1c
DoVanThanh <dvt2010200478@gmail.com>
2025-02-12 12:19:33 +0700

    Màn hình đăng nhập, đăng ký và màn hình chào

--- app/src/main/java/com/md09/pharmapoly/welcome_login/DangKy.java
+++ app/src/main/java/com/md09/pharmapoly/welcome_login/DangKy.java
@@ -1,4 +1,4 @@
-package com.md09.pharmapoly;
+package com.md09.pharmapoly.welcome_login;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.md09.pharmapoly.R;
+
+public class DangKy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dang_ky);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(DangKy.this, Success.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
--- app/src/main/java/com/md09/pharmapoly/welcome_login/Success.java
+++ app/src/main/java/com/md09/pharmapoly/welcome_login/Success.java
@@ -1,8 +1,6 @@
-package com.md09.pharmapoly;
+package com.md09.pharmapoly.welcome_login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.md09.pharmapoly.R;
+
+public class Success extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_success);
+
     }
 }
