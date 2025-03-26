2f1aac3ec9f19f289d7a274e9610c9603a724f65
DoHoangLong2004 <longdh204@gmail.com>
2025-03-26 15:14:43 +0700

    update search and interface ...

--- app/src/main/java/com/md09/pharmapoly/ui/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/md09/pharmapoly/ui/view/fragment/ProfileFragment.java
@@ -56,6 +56,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -71,6 +72,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
